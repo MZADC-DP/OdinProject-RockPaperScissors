daf7683ef15ba7f2943a266bd5b66f9888b9ffb9
MZADC-DP <mzadc18@gmail.com>
2023-05-22 16:27:57 +0800

    Completed The Base Rock Paper Scissor Project

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # OdinProject-RockPaperScissors
+
+Lession Overview
+    1. 3 Ways to include JavaScript
+        1. Inline 
+        Example:
+        <head>
+            <title>Inline JavaScript</title>
+            <script>
+                function myFunction() {
+                    alert('Hello, World!');
+                }
+            </script>
+        </head>
+        2. Internal
+        Example:
+        <head>
+            <title>Internal JavaScript</title>
+            <script src="script.js"></script>
+        </head>
+        <body>
+            <button onclick="myFunction()">Click me</button>
+        </body>
+
+        3. External
+        Example:
+        <head>
+            <title>External JavaScript</title>
+            <script src="script.js"></script>
+        </head>
+        <body>
+            <button onclick="myFunction()">Click me</button>
+        </body>
---
+++ script.js
@@ -0,0 +1,76 @@
+function getComputerChoice () {
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissor = "Scissor";
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber == 0)
+    {
+        return rock;
+    }
+    else if (randomNumber == 1)
+    {
+        return paper;
+    }
+    else 
+    {
+        return scissor;
+    }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+
+function playRound(playerSelection, computerSelection) {
+    
+
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase())
+    {
+        return "Tied!";
+    }
+    else if ( (playerSelection.toLowerCase() == "rock" && computerChoice.toLowerCase() == "paper") || (playerSelection.toLowerCase() == "paper" && computerChoice.toLowerCase() == "scissor") || (playerSelection.toLowerCase() == "scissor" && computerChoice.toLowerCase() == "rock") )
+    {
+        // return "You Lose! " + computerChoice + " beats " + playerSelection;
+        return "Computer";
+    }
+    else 
+    {
+       // return "You Win! " + playerSelection+ " beats " + computerChoice;
+       return "Playerrock";
+    }
+ 
+}
+
+
+function game ()
+{
+    let userInput = prompt("Pick 1: Rock Paper Scissor");
+    let playGame = playRound(userInput, computerChoice);
+
+    if (playGame == "Player")
+    {
+        return "Player Wins";
+    }
+    else if (playGame == "Computer")
+    {
+        return "Computer Wins"
+    }
+    else 
+    {
+        return "No one wins";
+    }
+
+    console.log(playGame);
+    console.log(playGame);
+    console.log(playGame);
+    console.log(playGame);
+    console.log(playGame);
+
+
+
+}
+
+console.log(game());
+
